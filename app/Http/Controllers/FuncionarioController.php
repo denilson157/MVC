64951dc0b1d019206591d78fa87069116af7df37
Denílson Pereira <dpereira@actioncargo.com.br>
2021-05-14 21:49:41 -0300

    fim da aula

--- app/Http/Controllers/FuncionarioController.php
+++ app/Http/Controllers/FuncionarioController.php
@@ -5,11 +5,107 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Funcionario;
 
-
 class FuncionarioController extends Controller
 {
     public function index()
     {
-        return view('funcionario.index')->with('funcionarios', Funcionario::all());
+        return Funcionario::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $json = $request->getContent();
+
+        return Funcionario::create(json_decode($json, JSON_OBJECT_AS_ARRAY));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $funcionario = Funcionario::find($id);
+        if ($funcionario) {
+            return $funcionario;
+        } else
+            return json_encode([$id => "não existe"]);
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $funcionario = Funcionario::find($id);
+
+        if ($funcionario) {
+            $json = $request->getContent();
+            $funcAtualizar = json_decode($json, JSON_OBJECT_AS_ARRAY);
+            $funcionario->nome = $funcAtualizar['nome'];
+            $funcionario->email = $funcAtualizar['email'];
+            $funcionario->telefone = $funcAtualizar['telefone'];
+            $funcionario->endereco = $funcAtualizar['endereco'];
+
+            $retorno = $funcionario->update() ? [$id => 'Atualizado'] : [$id => 'Erro'];
+
+        } else
+            $retorno = json_encode([$id => "não existe"]);
+
+
+        return $retorno;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $funcionario = Funcionario::find($id);
+
+        if (!empty($funcionario)) {
+            $retorno = $funcionario->delete() ? [$id => 'Apagado'] : [$id => 'Erro'];
+        } else {
+            $retorno = [$id => 'Funcionário nao encontrado'];
+        }
+
+        return json_encode($retorno);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FuncionarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::prefix('v1')->group(static function () {
+    Route::get('/funcionarios',  [FuncionarioController::class, 'index']);
+    Route::post('/funcionarios',  [FuncionarioController::class, 'store']);
+    Route::delete('/funcionarios/{id}',  [FuncionarioController::class, 'destroy']);
+    Route::get('/funcionarios/{id}',  [FuncionarioController::class, 'show']);
+    Route::put('/funcionarios/{id}',  [FuncionarioController::class, 'update']);
 });
